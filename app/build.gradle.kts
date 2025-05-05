919e7997cf8f46564c637ae363192c8895b0af85
Kevin Paul Montealegre Melo <kmontealegremelo@gmail.com>
2025-05-05 17:20:16 +0100

    fix impl. libs retrofit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,8 +44,8 @@ dependencies {
 
     // Retrofit
 
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
 
 
     implementation(libs.androidx.core.ktx)
